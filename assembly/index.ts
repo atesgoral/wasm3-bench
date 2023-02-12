14d43f1249fde1038af26903fd2abb8fb2715a97
Ates Goral <ates@magnetiq.com>
2023-02-12 13:22:05 -0500

    Metaballs

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,3 +1,62 @@
+memory.grow(1);
+
+const COLS: i32 = 16;
+const ROWS: i32 = 16;
+
+const OFF: u8 = 0;
+const LIGHT_GRAY: u8 = 1;
+const DARK_GRAY: u8 = 2;
+const ON: u8 = 3;
+
+const ASPECT_RATIO: f32 = 13 / 19.25;
+
+function set(index: i32, color: u8): void {
+  store<u8>(index, color);
+}
+
+function setXY(x: i32, y: i32, color: u8): void {
+  store<u8>(x + y * COLS, color);
+}
+
+function clamp_f32(x: f32, min: f32, max: f32): f32 {
+  return Mathf.max(min, Mathf.min(max, x));
+}
+
+function smoothstep_f32(edge0: f32, edge1: f32, x: f32): f32 {
+  const t: f32 = clamp_f32((x - edge0) / (edge1 - edge0), 0.0, 1.0);
+  return t * t * (3 - 2 * t);
+}
+
+export function metaballs_f32(count: i32): f64 {
+  for (let i = 0; i < count; i++) {
+    const s: f32 = <f32>count / 60;
+
+    for (let y: i32 = 0; y < ROWS; y++) {
+      for (let x: i32 = 0; x < COLS; x++) {
+        let d: f32 = 0;
+
+        d += 3 / Mathf.hypot(
+          7.5 + Mathf.sin(s * 1.1) * 7.5 - <f32>x,
+          (7.5 + Mathf.cos(s * 1.3) * 7.5 - <f32>y) / ASPECT_RATIO
+        );
+
+        d += 2 / Mathf.hypot(
+          7.5 + Mathf.sin(s * 1.5) * 7.5 - <f32>x,
+          (7.5 + Mathf.cos(s * 1.7) * 7.5 - <f32>y) / ASPECT_RATIO
+        );
+
+        setXY(x, y, <u8>Mathf.round(smoothstep_f32(0.75, 1.0, d) * 3));
+      }
+    }
+  }
+
+  return 0;
+}
+
+/**
+ * Math
+ */
+
 export function loop_sin_f32(count: i32): f64 {
   let sum: f32 = 0;
 
